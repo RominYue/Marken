142a5fc8c148c3870823977ea63ec4ba504aca1b
cyberzhg <cyberzhg@gmail.com>
2014-09-01 20:46:54 +0800

    Parse inline code.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- Parser/parse_elem_code_inline.cpp
+++ Parser/parse_elem_code_inline.cpp
@@ -7,3 +7,46 @@ ParseElementCodeInline::ParseElementCodeInline() : ParseElementSpan() {
 ParseElementType ParseElementCodeInline::type() const {
     return ParseElementType::TYPE_CODE_INLINE;
 }
+
+int ParseElementCodeInline::tryParse(const string& text, int offset) {
+    if (text[offset] == '`') {
+        int index = offset;
+        int length = text.length();
+        this->_backtickNum = 1;
+        for (++index; index < length; ++index) {
+            if (text[index] == '`') {
+                ++this->_backtickNum;
+            } else {
+                break;
+            }
+        }
+        int num = 0;
+        bool escape = false;
+        for (++index; index < length; ++index) {
+            if (escape) {
+                escape = false;
+            } else {
+                if (text[index] == '`') {
+                    if (++num >= this->_backtickNum) {
+                        return index + 1 - offset;
+                    }
+                } else {
+                    num = 0;
+                    if (text[index] == '\\') {
+                        escape = true;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+string ParseElementCodeInline::generateOpenHtml() const  {
+    auto code = this->text.substr(this->_backtickNum, this->text.length() - this->_backtickNum * 2);
+    return "<code>" + this->translateAmp(code);
+}
+
+string ParseElementCodeInline::generateCloseHtml() const {
+    return "</code>";
+}
--- Parser/parse_elem_code_inline.h
+++ Parser/parse_elem_code_inline.h
@@ -7,6 +7,13 @@ class ParseElementCodeInline : public ParseElementSpan {
 public:
     ParseElementCodeInline();
     virtual ParseElementType type() const override final;
+
+    virtual int tryParse(const string& text, int offset) override final;
+    virtual string generateOpenHtml() const override final;
+    virtual string generateCloseHtml() const override final;
+
+private:
+    int _backtickNum;
 };
 
 #endif // PARSE_ELEM_CODE_INLINE_H
